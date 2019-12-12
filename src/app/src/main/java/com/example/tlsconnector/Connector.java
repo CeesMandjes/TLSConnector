5a0785f4be18c6ae985a2022281b3e87cfc87072
CeesMandjes <ceesmandjes@gmail.com>
2019-12-12 14:34:32 +0100

    Create HTTPS connection with Google and print page result on screen

--- src/app/src/main/java/com/example/tlsconnector/Connector.java
+++ src/app/src/main/java/com/example/tlsconnector/Connector.java
@@ -1,4 +1,66 @@
 package com.example.tlsconnector;
 
-public class Connector {
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class Connector extends AsyncTask<String, String, String> {
+
+    private TextView textView;
+
+    public Connector(TextView printResult)
+    {
+        this.textView = printResult;
+    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+        HttpsURLConnection conn = null;
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urls[0]);
+            conn = (HttpsURLConnection) url.openConnection();
+            conn.connect();
+
+            InputStream stream = conn.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuffer buffer = new StringBuffer();
+
+            String line = "";
+            while((line = reader.readLine()) != null){
+                buffer.append(line);
+            }
+
+            return buffer.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(conn != null)
+                conn.disconnect();
+            try {
+                if(reader != null)
+                    reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        textView.setText(result);
+    }
 }
--- src/app/src/main/java/com/example/tlsconnector/MainActivity.java
+++ src/app/src/main/java/com/example/tlsconnector/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.tlsconnector;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private TextView textView;
+    private String[] tlsVersions = new String[]{"TLS 1.0", "TLS 1.1", "TLS 1.2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.tls_version_spin);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,tlsVersions);
+        spinner.setAdapter(adapter);
+    }
+
+    public void performTLSHandshake(View view)
+    {
+        textView = findViewById(R.id.conn_details_tv);
+        new Connector(textView).execute("https://google.com/");
     }
 }
