ffa8cc2bcb7947eac428cf3c784f19600d7df687
Cees Mandjes <ceesmandjes@gmail.com>
2019-12-13 17:07:16 +0100

    Implemented OkHttp library

--- src/app/src/main/java/com/example/tlsconnector/MainActivity.java
+++ src/app/src/main/java/com/example/tlsconnector/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,11 +21,23 @@ import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner;
     private TextView textView;
     private String[] tlsVersions = new String[]{"TLS 1.0", "TLS 1.1", "TLS 1.2"};
+
+
+    private Button okHttpBtn;
+    private OkHttpClient okHttpClient;
+    private Request request;
+    private String url = "https://google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +53,28 @@ public class MainActivity extends AppCompatActivity {
     public void performTLSHandshake(View view)
     {
         textView = findViewById(R.id.conn_details_tv);
-        new Connector(textView).execute("https://google.com/");
+        new Connector(textView).execute("https://revoked.badssl.com/");
+    }
+
+    public void okHttp(View view)
+    {
+        textView = findViewById(R.id.conn_details_tv);
+        okHttpBtn = findViewById(R.id.okhttp_btn);
+
+        okHttpClient = new OkHttpClient();
+        request = new Request.Builder()
+                .url(url).build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                textView.setText(e.toString());
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                textView.setText(response.body().string());
+            }
+        });
     }
 }
