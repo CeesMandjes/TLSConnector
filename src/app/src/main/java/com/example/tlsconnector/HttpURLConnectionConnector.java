f0a24aca219565ac35e54b468b12cfa25dd9ee04
Cees Mandjes <ceesmandjes@gmail.com>
2020-02-10 10:43:22 +0100

    Improved comments in source code for HttpURLConnectionConnector

--- src/app/src/main/java/com/example/tlsconnector/HttpURLConnectionConnector.java
+++ src/app/src/main/java/com/example/tlsconnector/HttpURLConnectionConnector.java
@@ -39,6 +39,7 @@ public class HttpURLConnectionConnector extends AsyncTask<String, Void, Void> {
     protected Void doInBackground(final String... urls) {
         try
         {
+            //** Pin certificate **
             //Load given certificate into certificate factory and create certificate object
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
             Certificate ca = cf.generateCertificate(certificate);
@@ -58,36 +59,38 @@ public class HttpURLConnectionConnector extends AsyncTask<String, Void, Void> {
             session = SSLContext.getInstance("TLS");
             session.init(null, tmf.getTrustManagers(), null);
 
-            // Android Safetynet checks
-            //Get Nonce
+            //** Get nonce from server **
             URL getNonceUrl = new URL(urls[0] + "api/getnonce");
             HttpsURLConnection getNonceRequest =
                     (HttpsURLConnection) getNonceUrl.openConnection();
+            //Use pinned certificate
             getNonceRequest.setSSLSocketFactory(session.getSocketFactory());
             String nonce = returnVal(new BufferedInputStream(getNonceRequest.getInputStream()));
             mainContext.addTextToOutputUI(TAG , "URL: " + getNonceRequest.getURL().toString() + "\nPinned certificate: correct \nNonce:" + nonce);
 
-            //Get JWS response from Google
+            //** Get signed attestation from Google **
             AndroidSafetyNet androidSafetyNet = new AndroidSafetyNet(mainContext);
-            String jwsResponse = androidSafetyNet.getJws(nonce.getBytes());
+            //Do request with given nonce
+            String signedAttestation = androidSafetyNet.getJws(nonce.getBytes());
             mainContext.addTextToOutputUI("Google SafetyNet" , "JWS response received");
 
-            //Send JWS to server and wait for response
+            //** Forward signed attestation to server **
             URL sendSafetyNetJWS = new URL(urls[0] + "api/validatejws");
             HttpsURLConnection validateJWSRequest =
                     (HttpsURLConnection) sendSafetyNetJWS.openConnection();
+            //Set signed attestation in POST request
             validateJWSRequest.setDoOutput(true);
             validateJWSRequest.setRequestMethod("POST");
             validateJWSRequest.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            String postParamaters = "jws=" + jwsResponse;
-            validateJWSRequest.setFixedLengthStreamingMode(postParamaters.getBytes().length);
+            String postParameters = "jws=" + signedAttestation;
+            validateJWSRequest.setFixedLengthStreamingMode(postParameters.getBytes().length);
             PrintWriter out = new PrintWriter(validateJWSRequest.getOutputStream());
-            out.print(postParamaters);
+            out.print(postParameters);
             out.close();
+            //Use pinned certificate
             validateJWSRequest.setSSLSocketFactory(session.getSocketFactory());
             validateJWSRequest.connect();
             mainContext.addTextToOutputUI(TAG , "URL: " + validateJWSRequest.getURL().toString() + "\nResult: " + returnVal(new BufferedInputStream(validateJWSRequest.getInputStream())));
-
 
         } catch (CertificateException e) {
             mainContext.addErrorToOutputUI(TAG, e.getMessage());
